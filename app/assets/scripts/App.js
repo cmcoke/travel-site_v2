dc21cb2445e6577093cf29e25dbd6a9c4994b5df
cmcoke <minoru.cm@gmail.com>
2019-12-22 14:19:58 -0500

    completed desktop and mobile header styles

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,23 @@
 import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu'
+
+
+
+let mobileMenu = new MobileMenu();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // allows webpack-dev-server to be executed by allowing hot module replacements and accepts updates on the fly
 if(module.hot) {
