2367dd7bf6e8d5c40fd30a6b70692ceadd7f0cb4
cmcoke <minoru.cm@gmail.com>
2019-12-22 21:33:41 -0500

    completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -9,19 +9,22 @@ let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
+new MobileMenu();
+let modal
 
-
-
-
-
-
-
-
-
-
-
-
-
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
 
 
 // allows webpack-dev-server to be executed by allowing hot module replacements and accepts updates on the fly
